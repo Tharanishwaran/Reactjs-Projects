f904cfe9bf99499a4f417b8353dde122755bce2e
Tharanishwara <tharanishwaran.ta@gmail.com>
2024-06-30 23:26:28 +0530

    styles added

--- QR-code/src/Qrcode.jsx
+++ QR-code/src/Qrcode.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import myimage from './assets/imageDesert.jpg'
 
 function Qrcode() {
  
     return (
-    <div>
+    <div className="app-container">
+      <h1>QR Code Generator</h1>
+      <img src={myimage} height={300} width={350} className='qr-image'></img>
+      <div>
+        <label htmlFor='dataInput' className='input-label'>
+          Data for QR code:
+        </label>
+
+      <input type='text' id='dataInput'></input>
+
+      <label htmlFor='sizeInput' className='input-label'>
+        Image size  :
+      </label>
+     <input type='text' id='sizeInput'></input>
+
+      <button className='generateb'>Generate QR Code</button>
+      <button className='downloadb'>Download QR code</button> 
+      </div>
  
-      <h1>qr code hello</h1>
+    
 
     </div>
   )
